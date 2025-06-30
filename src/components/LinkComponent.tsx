49589e27f4ea11dc62f61e7d9a1a857bb4d25d62
Enitan-dev <imoageleemmanuella@gmail.com>
2025-06-30 13:32:18 +0100

    Fix login button

---
+++ src/components/LinkComponent.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+type ButtonProps = {
+  label: string;
+  type?: "primary" | "outline";
+  onClick?: () => void;
+  href: string
+};
+
+export default function LinkComponent({ label, type = "primary", href, onClick }: ButtonProps) {
+  const base = "px-6 py-2 rounded-lg font-medium transition duration-300";
+  const primary = "bg-indigo-500 text-white hover:bg-indigo-600";
+  const outline = "border border-indigo-500 text-indigo-500 hover:bg-indigo-50";
+
+  const classes = `${base} ${type === "primary" ? primary : outline}`;
+  return <Link href={href} className={classes}>{label}</Link>;
+}
