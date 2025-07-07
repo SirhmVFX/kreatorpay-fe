eb02d68130215e1dbd7d9e3d987efb5ef2ab6fa4
Enitan-dev <imoageleemmanuella@gmail.com>
2025-07-07 12:45:18 +0100

    ff

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -5,7 +5,7 @@ import ForgotPassword from "@/components/ForgotPassword";
 export default function ForgotPasswordPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
-      <ForgotPassword />
+      <ForgotPassword /> 
     </div>
   );
 }
